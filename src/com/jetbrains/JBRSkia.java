f5b27a2bc53282a2ce2988a555d3a869d394d956
Sebastiano Poggi <poggos@gmail.com>
2026-06-22 09:44:20 +0200

    Mirror full image restore n command API

--- src/com/jetbrains/JBRSkia.java
+++ src/com/jetbrains/JBRSkia.java
@@ -538,6 +538,8 @@ public interface JBRSkia {
     long COMMAND_CAP64_HIGH_SAVE_TRANSLATE_LAYER_SAVE_TRANSLATE = Long.parseLong("17179869184");
     /** Supports compact full-source image reference draw plus restore records. */
     long COMMAND_CAP64_HIGH_DRAW_IMAGE_REF_FULL_RESTORE = Long.parseLong("34359738368");
+    /** Supports compact full-source image reference draw plus restoreN records. */
+    long COMMAND_CAP64_HIGH_DRAW_IMAGE_REF_FULL_RESTORE_N = Long.parseLong("68719476736");
 
     /**
      * Command-list operation: clear/fill the destination with one ARGB color.
@@ -955,6 +957,10 @@ public interface JBRSkia {
      * Command-list operation: draw a full-source image reference, then restore one save.
      */
     int COMMAND_DRAW_IMAGE_REF_FULL_RESTORE = Integer.parseInt("86");
+    /**
+     * Command-list operation: draw a full-source image reference, then restore multiple saves.
+     */
+    int COMMAND_DRAW_IMAGE_REF_FULL_RESTORE_N = Integer.parseInt("87");
 
     /**
      * Effect descriptor type: tint color filter.
@@ -1412,6 +1418,9 @@ public interface JBRSkia {
          *     nestedDx1000, nestedDy1000]}</li>
          *     <li>{@link JBRSkia#COMMAND_DRAW_IMAGE_REF_FULL_RESTORE}: {@code [op, 36, imageFlags,
          *     dstLeft1000, dstTop1000, dstRight1000, dstBottom1000, cacheKeyHigh, cacheKeyLow]}</li>
+         *     <li>{@link JBRSkia#COMMAND_DRAW_IMAGE_REF_FULL_RESTORE_N}: {@code [op, 40, imageFlags,
+         *     dstLeft1000, dstTop1000, dstRight1000, dstBottom1000, cacheKeyHigh, cacheKeyLow,
+         *     extraRestoreCount]}</li>
          *     <li>{@link JBRSkia#COMMAND_CLIP_PATH}: {@code [op, 24 + pathDataLength * 4, flags,
          *     clipOp, fillType, pathDataLength, pathVerb0, ...]}, where path data is a sequence of
          *     {@code COMMAND_PATH_VERB_*} records using fixed-point coordinates scaled by 1000.</li>
